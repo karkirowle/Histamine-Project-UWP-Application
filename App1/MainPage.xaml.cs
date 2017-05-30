43251c312880e5a2c7de626aa149e17057cf4839
karkirowle <szerk.animeblog@gmail.com>
2017-05-30 01:03:40 +0100

    Version 0.2
    Editing out the remaing useless code which was for the HM-10 apparatus
    and confirming that a two-way communication interface works.

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -23,22 +23,20 @@ using System.Threading.Tasks;
 namespace App1
 {
     /// <summary>
-    /// Histamine Control Pane version 0.1 ALPHA
-    /// Program on startup retrieves necessary objectst to send a message to the histamine sensing apparatus.
+    /// Histamine Control Panel version 0.2 Alpha
+    /// Implements two-way Bluetooth communication interface for the basis of histamine sensing apparatus
+    /// 
     /// Bence Halpern
-    /// 2017.04.01.
+    /// 2017.05.30.
     /// </summary>
     /// 
    
     public sealed partial class MainPage : Page
     {
 
-        public List<string> deviceList = new List<string>();
-        GattDeviceService bleService;
+       public List<string> deviceList = new List<string>();
         GattCharacteristic accData;
         GattCharacteristic accConfig;
-        DevicePairingResult dpr;
-        bool paired = false;
         bool firstUpdate = true;
 
 
@@ -51,12 +49,6 @@ namespace App1
         }
         private async void LoadedState()
         {
-
-          /*  while (await pairHandler() == "Disconnected")
-            {
-                Debug.WriteLine("Connection failed... Trying again.");
-            } */
-           //enumerateSnapshot();
             var taskDevices = await getDevices();
             Debug.WriteLine("G");
            sendMessage("Hello World");
@@ -64,27 +56,12 @@ namespace App1
         public async Task<List<string>> getDevices()
         {
             Debug.WriteLine("C");
-            /* foreach (DeviceInformation di in await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(new Guid("0000ffe0-0000-1000-8000-00805f9b34fb"))))
-             {
-                 Debug.WriteLine("D");
-                 Debug.WriteLine(di.Name);
-                 Debug.WriteLine(di.Id);
-                 Debug.WriteLine(di.IsEnabled);
-                 GattDeviceService bleService = await GattDeviceService.FromIdAsync(di.Id);
-                 Debug.WriteLine(bleService.Device.ConnectionStatus.ToString());
-                 Debug.WriteLine("E");
-                 Debug.WriteLine(GattCharacteristic.ConvertShortIdToUuid(0xFFE1).ToString());
-                 // TODO: Throws exception if not paired before startup because pairing info is not stored on device
-                 accConfig = bleService.GetCharacteristics(GattCharacteristic.ConvertShortIdToUuid(0xFFE1))[0];
-                 Debug.WriteLine("F");
-             } */
             foreach (DeviceInformation di in await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(new Guid("00002220-0000-1000-8000-00805f9b34fb"))))
             {
                 Debug.WriteLine("D");
                 Debug.WriteLine(di.Name);
                 GattDeviceService bleService = await GattDeviceService.FromIdAsync(di.Id);
                 Debug.WriteLine(bleService.Device.ConnectionStatus.ToString());
-                //Debug.WriteLine(GattCharacteristic.ConvertShortIdToUuid(0x2222).ToString());
                 accConfig = bleService.GetCharacteristics(GattCharacteristic.ConvertShortIdToUuid(0x2222))[0];
                 Debug.WriteLine("E:Write_char");
                 accData = bleService.GetCharacteristics(GattCharacteristic.ConvertShortIdToUuid(0x2221))[0];
@@ -116,60 +93,7 @@ namespace App1
                 textBox.Text += "\n";
             });
         }
-        async Task<string> pairHandler()
-        {
-            DeviceInformationCollection collection = await DeviceInformation.FindAllAsync(BluetoothLEDevice.GetDeviceSelectorFromDeviceName("HIST"));
-            DeviceInformation foundDevice = collection[0];
-            BluetoothLEDevice egydevice = await BluetoothLEDevice.FromIdAsync(foundDevice.Id);
-            if (egydevice.ConnectionStatus.ToString() == "Disconnected")
-            {
-                dpr = await foundDevice.Pairing.PairAsync(DevicePairingProtectionLevel.None);
-                return egydevice.ConnectionStatus.ToString();
-            }
-            return egydevice.ConnectionStatus.ToString();
-        }
-        async void enumerateSnapshot()
-        {
-            // Finding HIST Device
-   
-            DeviceInformationCollection collection = await DeviceInformation.FindAllAsync(BluetoothLEDevice.GetDeviceSelectorFromPairingState(true));
-            Debug.WriteLine(collection.Count());
-            if (collection.Count() != 1)
-            {
-                Debug.WriteLine("HIST sensor unpaired - pairing process started");
-                collection = await DeviceInformation.FindAllAsync(BluetoothLEDevice.GetDeviceSelectorFromDeviceName("HIST"));
-                for (int i = 0; i < collection.Count(); i++)
-                {
-                    if (collection[i].Name.ToString() == "HIST")
-                    {
-                        Debug.WriteLine("Found HIST sensor");
-                        DeviceInformation foundDevice = collection[i];
-                        dpr = await foundDevice.Pairing.PairAsync(DevicePairingProtectionLevel.None); // Pair
-                        Debug.WriteLine(foundDevice.Id);
-                        BluetoothLEDevice egydevice = await BluetoothLEDevice.FromIdAsync(foundDevice.Id);
-                        GattDeviceService egyservice = egydevice.GetGattService(new Guid("0000ffe0-0000-1000-8000-00805f9b34fb"));
-                        
-                        Debug.WriteLine(GattCharacteristic.ConvertShortIdToUuid(0xFFE1).ToString());
-                        Debug.WriteLine("success with dpr");
-                    
-                        Debug.WriteLine("Sucess with GATT");
 
-                    }
-                }
-            }
-            else
-            {
-                Debug.WriteLine("HIST already paired");
-                DeviceInformation foundDevice = collection[0];
-               // bleService = await BluetoothLEDevice.FromIdAsync(foundDevice.Id);
-                Debug.WriteLine(bleService.DeviceId);
-              //  Debug.WriteLine(bleService.ConnectionStatus.ToString());
-              //  Debug.WriteLine(bleService.GattServices);
-               // GattDeviceService gattService = bleService.GetGattService(GattCharacteristic.ConvertShortIdToUuid(0xFFE1));
-                Debug.WriteLine("Service done");
-            }
-            
-          }
         public async void sendMessage(string messageToSend)
         {
             // Worldlimit to send is 20 - longer messages need to be partitioned into 20
