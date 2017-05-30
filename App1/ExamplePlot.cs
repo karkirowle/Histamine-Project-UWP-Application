ed59add0ce0884c2df4c6e6162b6fd3404860cd1
karkirowle <szerk.animeblog@gmail.com>
2017-05-30 15:40:12 +0100

    Version 0.3 Alpha
    Contains addition of voltammetry plot. TODO: interface for voltammetry
    plot

---
+++ App1/ExamplePlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UniversalApp1
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -18,32 +18,50 @@ using Windows.Devices.Enumeration;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
 
 
 namespace App1
 {
     /// <summary>
-    /// Histamine Control Panel version 0.2 Alpha
+    /// Histamine Control Panel version 0.3 Alpha
     /// Implements two-way Bluetooth communication interface for the basis of histamine sensing apparatus
+    /// 
+    /// 0.3 - Contains the OxyPlot module to visualise voltammetry. TODO: Make an interface for any voltammetry data series accounting for thread management.
     /// 
     /// Bence Halpern
     /// 2017.05.30.
     /// </summary>
     /// 
-   
+
     public sealed partial class MainPage : Page
     {
 
-       public List<string> deviceList = new List<string>();
+        public List<string> deviceList = new List<string>();
         GattCharacteristic accData;
         GattCharacteristic accConfig;
         bool firstUpdate = true;
-
+        double[] x_v = { -0.4, -0.38994, -0.37987, -0.36981, -0.35975, -0.34969, -0.33962, -0.32956, -0.3195, -0.30943, -0.29937, -0.28931, -0.27925, -0.26918, -0.25912, -0.24906, -0.23899, -0.22893, -0.21887, -0.20881, -0.19874, -0.18868, -0.17862, -0.16855, -0.15849, -0.14843, -0.13836, -0.1283, -0.11824, -0.10818, -0.098113, -0.08805, -0.077987, -0.067925, -0.057862, -0.047799, -0.037736, -0.027673, -0.01761, -0.0075472, 0.0025157, 0.012579, 0.022642, 0.032704, 0.042767, 0.05283, 0.062893, 0.072956, 0.083019, 0.093082, 0.10314, 0.11321, 0.12327, 0.13333, 0.1434, 0.15346, 0.16352, 0.17358, 0.18365, 0.19371, 0.20377, 0.21384, 0.2239, 0.23396, 0.24403, 0.25409, 0.26415, 0.27421, 0.28428, 0.29434, 0.3044, 0.31447, 0.32453, 0.33459, 0.34465, 0.35472, 0.36478, 0.37484, 0.38491, 0.39497, 0.39497, 0.38491, 0.37484, 0.36478, 0.35472, 0.34465, 0.33459, 0.32453, 0.31447, 0.3044, 0.29434, 0.28428, 0.27421, 0.26415, 0.25409, 0.24403, 0.23396, 0.2239, 0.21384, 0.20377, 0.19371, 0.18365, 0.17358, 0.16352, 0.15346, 0.1434, 0.13333, 0.12327, 0.11321, 0.10314, 0.093082, 0.083019, 0.072956, 0.062893, 0.05283, 0.042767, 0.032704, 0.022642, 0.012579, 0.0025157, -0.0075472, -0.01761, -0.027673, -0.037736, -0.047799, -0.057862, -0.067925, -0.077987, -0.08805, -0.098113, -0.10818, -0.11824, -0.1283, -0.13836, -0.14843, -0.15849, -0.16855, -0.17862, -0.18868, -0.19874, -0.20881, -0.21887, -0.22893, -0.23899, -0.24906, -0.25912, -0.26918, -0.27925, -0.28931, -0.29937, -0.30943, -0.3195, -0.32956, -0.33962, -0.34969, -0.35975, -0.36981, -0.37987, -0.38994, -0.4 };
+        double[] y_v = { 0, -1.9614e-06, -2.6117e-06, -3.754e-06, -5.4536e-06, -8.0848e-06, -1.1938e-05, -1.7816e-05, -2.6083e-05, -3.8579e-05, -5.7197e-05, -8.4343e-05, -0.00012531, -0.00018488, -0.00027339, -0.00040375, -0.00059661, -0.00088658, -0.0013121, -0.0019412, -0.0028707, -0.0042436, -0.0063095, -0.0093189, -0.013744, -0.020292, -0.029929, -0.044308, -0.064985, -0.095589, -0.14016, -0.20409, -0.29582, -0.42305, -0.59942, -0.8317, -1.112, -1.4613, -1.8329, -2.1866, -2.4811, -2.6732, -2.7582, -2.7386, -2.648, -2.5156, -2.3637, -2.2117, -2.068, -1.9387, -1.8233, -1.7222, -1.634, -1.5569, -1.4894, -1.4299, -1.3772, -1.3303, -1.2881, -1.25, -1.2154, -1.1837, -1.1546, -1.1277, -1.1027, -1.0794, -1.0577, -1.0373, -1.0181, -0.99997, -0.98284, -0.9666, -0.95117, -0.93648, -0.92249, -0.90912, -0.89633, -0.88409, -0.87235, -0.86107, -0.85023, -0.83981, -0.82976, -0.82007, -0.81073, -0.8017, -0.79297, -0.78452, -0.77635, -0.76842, -0.76074, -0.75328, -0.74604, -0.73899, -0.73214, -0.72547, -0.71896, -0.71259, -0.70636, -0.70023, -0.69418, -0.68816, -0.68212, -0.67597, -0.66961, -0.66287, -0.6555, -0.64718, -0.63741, -0.62552, -0.61057, -0.59123, -0.56582, -0.53227, -0.48847, -0.43269, -0.36356, -0.28365, -0.19744, -0.1118, -0.035265, 0.024542, 0.065369, 0.089052, 0.09746, 0.096255, 0.089028, 0.078947, 0.068194, 0.05752, 0.047926, 0.039494, 0.032328, 0.026298, 0.021323, 0.01721, 0.013859, 0.011119, 0.0088865, 0.0070705, 0.0055894, 0.0043815, 0.0033935, 0.002585, 0.0019211, 0.0013756, 0.00092616, 0.00055545, 0.00024917, -4.0847e-06, -0.00021347, -0.00038661, -0.00052967, -0.0006477, -0.00074484, -0.00082449, -0.0008895, -0.00094219, -0.00098451, -0.0010181};
+    
 
         public MainPage()
         {
             this.InitializeComponent();
             Debug.WriteLine("A");
+            
+            MyModel = new PlotModel { Title = "Cyclic Voltammetry" };
+            // var scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle };
+            var LineSeries = new LineSeries{};
+            for (int i = 0; i < x_v.Length; i++)
+            {
+                //scatterSeries.Points.Add(new ScatterPoint(x_v[i], y_v[i]));
+                LineSeries.Points.Add(new DataPoint(x_v[i], y_v[i]));
+                //MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            }
+            MyModel.Series.Add(LineSeries);
+            MyPlotView.Model = MyModel;
             LoadedState();
             Debug.WriteLine("B");
         }
@@ -52,6 +70,7 @@ namespace App1
             var taskDevices = await getDevices();
             Debug.WriteLine("G");
            sendMessage("Hello World");
+            
         }
         public async Task<List<string>> getDevices()
         {
@@ -114,6 +133,14 @@ namespace App1
             for (int i = 0; i < str.Length; i += maxChunkSize)
                 yield return str.Substring(i, Math.Min(maxChunkSize, str.Length - i));
         }
+
+        public void MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel { get; private set; }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
